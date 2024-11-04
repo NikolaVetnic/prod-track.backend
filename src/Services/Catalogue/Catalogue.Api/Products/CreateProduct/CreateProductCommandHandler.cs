dba35133ee6d8c5bda985377912ce89f46689d31
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-11-04 07:57:29 +0100

    feat: implement create product handler & endpoint

---
+++ src/Services/Catalogue/Catalogue.Api/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace Catalogue.Api.Products.CreateProduct;
+
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Price = command.Price,
+            PurchaseDate = command.PurchaseDate,
+            Description = command.Description,
+        };
+
+        //save to database
+
+        return new CreateProductResult(Guid.NewGuid());
+    }
+}
+
+public record CreateProductCommand(
+    string Name,
+    List<string> Category,
+    decimal Price,
+    DateTime PurchaseDate,
+    string Description) : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
---
+++ src/Services/Catalogue/Catalogue.Api/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,30 @@
+﻿namespace Catalogue.Api.Products.CreateProduct;
+
+public class CreateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/products",
+            async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<CreateProductResponse>();
+
+                return Results.Created($"/products/{response.Id}", response);
+            })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResult>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product");
+    }
+}
+
+public record CreateProductRequest(string Name,
+    List<string> Category,
+    decimal Price,
+    DateTime PurchaseDate,
+    string Description);
+
+public record CreateProductResponse(Guid Id);
