7cce162621fb9a43f137af1a4d0381ceb817f861
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-11-05 23:07:49 +0100

    feat: get product by id

---
+++ src/Services/Catalogue/Catalogue.Api/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,24 @@
+﻿namespace Catalogue.Api.Products.GetProductById;
+
+public class GetProductByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id}",
+                async (Guid id, ISender sender) =>
+                {
+                    var result = await sender.Send(new GetProductByIdQuery(id));
+                    var response = result.Adapt<GetProductByIdResponse>();
+
+                    return Results.Ok(response);
+                })
+            .WithName("GetProductsById")
+            .Produces<GetProductByIdResponse>()
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products By Id")
+            .WithDescription("Get Products By Id");
+    }
+}
+
+//public record GetProductByIdRequest();
+public record GetProductByIdResponse(Product Product);
